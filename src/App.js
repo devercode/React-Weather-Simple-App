9657cfb47eef5b2d9f036bcd3b9867f9c92c8c8a
Devercode <root@devercode.dev>
2022-05-02 12:25:43 +0700

    feat: toggleTheme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Card } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actions } from "./state/slices/theme";
 
 function App() {
+  const dispatch = useDispatch();
+  const themeMode = useSelector((state) => state.theme.mode);
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: themeMode,
+        },
+      }),
+    [themeMode]
+  );
+
+  const toggleTheme = () => {
+    dispatch(actions.toggle());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>This is using the dark mode {themeMode}</div>
+      <Button variant="contained" onClick={toggleTheme}>
+        Toggle
+      </Button>
+      <Card>Hello</Card>
+    </ThemeProvider>
   );
 }
 
